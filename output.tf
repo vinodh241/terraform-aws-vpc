9e37db9cd3c50f8ddb0f6d6348d000ec9255fa0e
vinodh241 <a.vinodh9072@gmail.com>
2025-06-29 12:09:32 +0530

    terraform-aws-vpc

--- output.tf
+++ output.tf
@@ -3,3 +3,17 @@ output "vpc_id" {
 value = aws_vpc.main.id
   
 }
+
+output "private_subnet_ids" {
+    value = aws_subnet.private[*].id
+  
+}
+output "public_subnet_ids" {
+    value = aws_subnet.public[*].id
+  
+}
+
+output "database_subnet_ids" {
+    value = aws_subnet.database[*].id
+  
+}
